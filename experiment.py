27ba436f2479772a1cf75b8398c432c6854a9dcb
smashley-addisson <smashley.addisson@gmail.com>
2017-07-05 13:52:08 -0400

    adds stuff

--- experiment.py
+++ experiment.py
@@ -3,6 +3,9 @@ from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
 
+import json
+
+
 #Variables that contains the user credentials to access Twitter API 
 access_token = "2902033948-GUoceoyNVxjhklAsNxy6GIL6iGoo6YrR3PzhjN1"
 access_token_secret = "0ePHIbLO1S4gsIXPot1vMxx8PmFkiOTAjTq0rxr6134Kq"
@@ -13,8 +16,16 @@ consumer_secret = "f7B110Wq8bVPeADMdf9DwNHcBT3tbqOjMvZsM67eFCIdfNE724"
 #This is a basic listener that just prints received tweets to stdout.
 class StdOutListener(StreamListener):
 
+    def __init__(self, tweet_limit):
+        self.tweet_limit = tweet_limit
+        self.current_tweet_number = 1
+
     def on_data(self, data):
         print data
+        print "\n" + str(self.current_tweet_number) + "\n"
+        if self.current_tweet_number == self.tweet_limit:
+            return False
+        self.current_tweet_number += 1
         return True
 
     def on_error(self, status):
@@ -24,10 +35,14 @@ class StdOutListener(StreamListener):
 if __name__ == '__main__':
 
     #This handles Twitter authetification and the connection to Twitter Streaming API
-    l = StdOutListener()
+    l = StdOutListener(3) 
     auth = OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
     stream = Stream(auth, l)
 
-    #This line filter Twitter Streams to capture data by the keywords: 'python', 'javascript', 'ruby'
-    stream.filter(track=['python', 'javascript', 'ruby'])
+    #This filters Twitter Streams to capture data by the keywords from a config file (config.json)
+    
+    with open('config.json') as data_file:
+        data = json.load(data_file)
+        stream.filter(track=data["hashtags"])
+        
