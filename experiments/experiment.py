a20bd6c452981128f1774ac3b97c4dbdbafe9e89
Marco Cardacci <marco.cardacci@gmail.com>
2017-07-12 20:22:19 -0400

    figured out how to use virustotal

--- experiments/experiment.py
+++ experiments/experiment.py
@@ -6,14 +6,16 @@ from tweepy import Stream
 import json
 
 
-#Variables that contains the user credentials to access Twitter API 
+# Variables that contains the user credentials to access Twitter API 
 access_token = "2902033948-GUoceoyNVxjhklAsNxy6GIL6iGoo6YrR3PzhjN1"
 access_token_secret = "0ePHIbLO1S4gsIXPot1vMxx8PmFkiOTAjTq0rxr6134Kq"
 consumer_key = "oHSQXvTpsJ9oEL6pL1DAkOOxt"
 consumer_secret = "f7B110Wq8bVPeADMdf9DwNHcBT3tbqOjMvZsM67eFCIdfNE724"
 
+# Virustotal Credentials
+apikey = 'a4b128f4b05396d094ea6e08ddf37b895b05ccbe5d6f24389a197dfdd1f5aaa3' 
 
-#This is a basic listener that just prints received tweets to stdout.
+# This is a basic listener that just prints received tweets to stdout.
 class StdOutListener(StreamListener):
 
     def __init__(self, tweet_limit):
